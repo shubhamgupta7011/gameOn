9196197b67dd6416bd7781a354752fd972a7c3d1
Shubham Gupta <60481715+sankalp211@users.noreply.github.com>
2025-05-03 09:09:44 +0530

    structure

--- src/main/java/com/example/GameOn/service/OTPService.java
+++ src/main/java/com/example/GameOn/service/OTPService.java
@@ -11,19 +11,17 @@ import java.util.Random;
 public class OTPService {
 //    Recovery code :- K6LDH1786DHZJNRSUEJ42GQY
     // Twilio account SID and authentication token
-    private static final String ACCOUNT_SID = "AC402246e19c2d89bf2ef222ffd3271a01";
-    private static final String AUTH_TOKEN = "c73c28cbce047dd02b93c330c4a87a1f";
-    private static final String FROM_PHONE = "7011203385";
+
 
     public OTPService() {
-        Twilio.init(ACCOUNT_SID, AUTH_TOKEN);
+        Twilio.init("ACCOUNT_SID", "AUTH_TOKEN");
     }
 
     public void sendOTP(String phoneNumber, String otp) {
         // Send OTP via SMS
         Message message = Message.creator(
                 new PhoneNumber(phoneNumber),
-                new PhoneNumber(FROM_PHONE),
+                new PhoneNumber("FROM_PHONE"),
                 "Your OTP is: " + otp
         ).create();
 
